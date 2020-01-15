93c0af53b8fd968345fee1b77991fa93ce0d6ee3
CaiYueTing <qazxcv367tasi@gmail.com>
2020-01-15 16:26:07 +0800

    feat: add logging test level

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 from app import app
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('test_logger')
 
 if __name__ == '__main__':
+    logger.info('info level')
+    logger.debug('debug level')
+    logger.warning('warning level')
+    logger.error('error level')
+    logger.critical('critical level')
     app.run()
